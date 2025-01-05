83809bd195d0da9907efd78526191f589b7e8456
eunkang-choi <dmsrkd1216@gmail.com>
2025-01-05 22:50:14 +0900

    Chore: user, point, product 샘플데이터셋 sql쿼리문 작성

---
+++ src/main/resources/01_sample-user.sql
@@ -0,0 +1,14 @@
+INSERT INTO USER(name, email, password, gender, phone_number) VALUES
+('테스트', 'test1@yopmail.com', '1234', 'F', '01011112222');
+
+INSERT INTO USER(name, email, password, gender, phone_number) VALUES
+('유저1', 'user1@yopmail.com', '1234', 'M', '01021112222'),
+('유저2', 'user2@yopmail.com', 'abcd', 'F', '01022223333'),
+('유저3', 'user3@yopmail.com', 'password1', 'M', '01033334444'),
+('유저4', 'user4@yopmail.com', 'qwerty', 'F', '01044445555'),
+('유저5', 'user5@yopmail.com', 'test1234', 'M', '01055556666'),
+('유저6', 'user6@yopmail.com', 'mypassword', 'F', '01066667777'),
+('유저7', 'user7@yopmail.com', '12345678', 'M', '01077778888'),
+('유저8', 'user8@yopmail.com', 'abcdefg', 'F', '01088889999'),
+('유저9', 'user9@yopmail.com', 'zxcvbnm', 'M', '01099990000'),
+('유저10', 'user10@yopmail.com', 'qwertyuiop', 'F', '01000001111');
---
+++ src/main/resources/03_sample-product.sql
@@ -0,0 +1,14 @@
+INSERT INTO product(current_stock, description, merchant_email, merchant_name, merchant_phone_number, name, price)
+VALUES (1000, '테스트 상품입니다.', 'mtest1@yopmail.com', '판매자 1', '01099998888', '상품1', 3500);
+
+INSERT INTO product(current_stock, description, merchant_email, merchant_name, merchant_phone_number, name, price) VALUES
+(100, '테스트 상품 1입니다.', 'merchant1@yopmail.com', '판매자 1', '01099998888', '상품1', 3500),
+(200, '테스트 상품 2입니다.', 'merchant2@yopmail.com', '판매자 2', '01088887777', '상품2', 4500),
+(300, '테스트 상품 3입니다.', 'merchant3@yopmail.com', '판매자 3', '01077776666', '상품3', 5500),
+(400, '테스트 상품 4입니다.', 'merchant4@yopmail.com', '판매자 4', '01066665555', '상품4', 6500),
+(500, '테스트 상품 5입니다.', 'merchant5@yopmail.com', '판매자 5', '01055554444', '상품5', 7500),
+(600, '테스트 상품 6입니다.', 'merchant6@yopmail.com', '판매자 6', '01044443333', '상품6', 8500),
+(700, '테스트 상품 7입니다.', 'merchant7@yopmail.com', '판매자 7', '01033332222', '상품7', 9500),
+(800, '테스트 상품 8입니다.', 'merchant8@yopmail.com', '판매자 8', '01022221111', '상품8', 10500),
+(900, '테스트 상품 9입니다.', 'merchant9@yopmail.com', '판매자 9', '01011110000', '상품9', 11500),
+(1000, '테스트 상품 10입니다.', 'merchant10@yopmail.com', '판매자 10', '01000009999', '상품10', 12500);
