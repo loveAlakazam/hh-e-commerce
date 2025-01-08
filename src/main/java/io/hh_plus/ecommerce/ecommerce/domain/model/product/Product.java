cf0b52f199299c30bb9877079f6cf10217282423
eunkang-choi <dmsrkd1216@gmail.com>
2025-01-08 16:19:40 +0900

    feat: 상품목록조회 API 추가 - 상품목록조회 컨트롤러/서비스 로직 추가 - 상품목록조회 pagination 응답리스폰스 정의 - 상품엔티티 정책 추가: 페이지네이션 고정 limit값(페이지당 최대 10개데이터만 응답) - 상품관련 에러코드 추가: 페이지번호 양수

--- src/main/java/io/hh_plus/ecommerce/ecommerce/domain/model/product/Product.java
+++ src/main/java/io/hh_plus/ecommerce/ecommerce/domain/model/product/Product.java
@@ -16,6 +16,9 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product extends BaseEntity {
+    // 페이지 목록당 고정 limit 정의
+    public static final int PAGINATION_LIMIT = 10;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY )
     private long id;
@@ -45,13 +48,35 @@ public class Product extends BaseEntity {
     @OneToOne(mappedBy= "product" )
     private OrderItem orderitem;
 
+    // 생성자
+    public Product(long id, String name, int price, String description, int currentStock, String merchantName, String merchantEmail, String merchantPhoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.currentStock = currentStock;
+        this.merchantName = merchantName;
+        this.merchantEmail = merchantEmail;
+        this.merchantPhoneNumber = merchantPhoneNumber;
+    }
+
     // 검증함수
     /**
      * validateProductId : 상품 식별자 검증함수
-     * @param productId long
+     * - 검증내용: 상품 식별자(Product.id)는 양수이다.
      *
+     * @param productId long
      */
     public static void validateProductId(long productId) {
         if(productId <= 0) throw new InvalidRequestException(ProductErrorCode.PRODUCT_ID_POSITIVE_NUMBER_POLICY);
+    }
+    /**
+     * validatePage : 상품목록 페이지 검증함수
+     * - 검증내용: 상품목록 페이지(page)는 양수이다.
+     *
+     * @param page int
+     */
+    public static void validatePage(int page) {
+        if(page <= 0) throw new InvalidRequestException(ProductErrorCode.PRODUCT_PAGE_POSITIVE_NUMBER_POLICY);
     }
 }
--- src/main/java/io/hh_plus/ecommerce/ecommerce/domain/service/product/ProductService.java
+++ src/main/java/io/hh_plus/ecommerce/ecommerce/domain/service/product/ProductService.java
@@ -1,9 +1,11 @@
 package io.hh_plus.ecommerce.ecommerce.domain.service.product;
 
+import io.hh_plus.ecommerce.ecommerce.domain.model.common.PageResponse;
 import io.hh_plus.ecommerce.ecommerce.domain.service.product.dto.response.ProductResponse;
 
 import java.util.Optional;
 
 public interface ProductService {
     ProductResponse getById(long productId);
+    PageResponse<ProductResponse> getProducts(int page);
 }
